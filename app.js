36b32eca4324a93f97ee832f127d712f87e69e8e
Joao HSO <joaosoliver@gmail.com>
2023-10-16 09:50:21 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o aplicativo de cadastro de jogos...")
 console.log("Aplicação se conectando ao banco de dados...")
 // mais um console log de testes
 console.log("O Paulo fez este commit sem você ver na aula")
+console.log("Enviando para o github...")
