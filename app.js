70e01fd1639026e17a5bbc8f1d4bf322f022d43f
Joao HSO <joaosoliver@gmail.com>
2023-10-16 09:42:42 -0300

    adicionando mais um console log de testes

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o aplicativo de cadastro de jogos...")
 console.log("Aplicação se conectando ao banco de dados...")
+// mais um console log de testes
+console.log("O Paulo fez este commit sem você ver na aula")
